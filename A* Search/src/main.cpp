77669607626143b3f68ed6e46dc8305d6d0833d0
SS47816 <ss1057532022@gmail.com>
2020-01-28 17:08:54 +0800

    feat: add Search and Compare functions

--- A* Search/src/main.cpp
+++ A* Search/src/main.cpp
@@ -10,7 +10,7 @@ using std::string;
 using std::vector;
 using std::abs;
 
-enum class State {kEmpty, kObstacle};
+enum class State {kEmpty, kObstacle, kClosed};
 
 
 vector<State> ParseLine(string line) {
@@ -28,7 +28,6 @@ vector<State> ParseLine(string line) {
   return row;
 }
 
-
 vector<vector<State>> ReadBoardFile(string path) {
   ifstream myfile (path);
   vector<vector<State>> board{};
@@ -42,13 +41,38 @@ vector<vector<State>> ReadBoardFile(string path) {
   return board;
 }
 
+// TODO: Write function to compare the f-value of two nodes here
+bool Compare(const vector<int> a, const vector<int> b) {
+  int f1 = a[2] + a[3];
+  int f2 = b[2] + b[3];
+  return f1 > f2;
+}
+
 // Calculate the manhattan distance
 int Heuristic(int x1, int y1, int x2, int y2) {
   return abs(x2 - x1) + abs(y2 - y1);
 }
 
+// TODO: Write the AddToOpen function here.
+void addToOpen(int x, int y, int g, int h, vector<vector<int>> &open_list, vector<vector<State>> &grid) {
+  open_list.pushback(vector<int>{x, y, g, h});
+  grid[x][y] = State::kClosed;
+}
+
 // TODO: Write the Search function stub here.
 vector<vector<State>> Search(vector<vector<State>> grid, int start[2], int goal[2]) {
+  // Create the vector of open nodes.
+  vector<vector<int>> open {};
+
+  // TODO: Initialize the starting node.
+  int x = start[0];
+  int y = start[1];
+  int g = 0;
+  int h = Heuristic(x, y, goal[0], goal[1]);
+
+  // TODO: Use AddToOpen to add the starting node to the open vector.
+  addToOpen(x, y, g, h, open, grid);
+
   cout << "No path found!" << "\n";
   return vector<vector<State>> {};
 }
