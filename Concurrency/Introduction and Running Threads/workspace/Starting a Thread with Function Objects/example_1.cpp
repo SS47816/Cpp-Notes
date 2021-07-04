e48e4ce707a871a01473d2f36ac202dfa1d9c395
SS47816 <ss1057532022@gmail.com>
2021-07-05 01:01:47 +0800

    feat: add Concurancy

---
+++ Concurrency/Introduction and Running Threads/workspace/Starting a Thread with Function Objects/example_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+
+class Vehicle
+{
+public:
+    void operator()()
+    {
+        std::cout << "Vehicle object has been created \n" << std::endl;
+    }
+};
+
+
+int main()
+{
+    // create thread 
+    std::thread t(Vehicle()); // C++'s most vexing parse
+
+    // do something in main()
+    std::cout << "Finished work in main \n";
+
+    // wait for thread to finish
+    t.join();
+
+    return 0;
+}
